6b330f034253fc1faf18f535c83c8cdc471147b9
Gregor Bell <gregorbell@virginmedia.com>
2018-10-07 20:45:11 +0100

    Completed chapter 6 of the workbook. Added relevant files and folders to the Chapter_6 folder.

---
+++ Chapter_6/6_1_3_2.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+	return str(first+second)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
+
+@app.route("/combine/<string:first>/<string:second>")
+def combine(first, second):
+	return str(first+second)
